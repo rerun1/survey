b9b5b554316c87b4b656bc0cabe847b8e435de1a
Suzi Rubino <suzi@suzirubi.com>
2019-07-30 23:07:00 -0700

    add jquery script to gather user answers into arrays

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,83 @@ $(document).ready(function(){
     var user5 = $("input[name=question5]:checked").val();
     var user6 = $("input[name=question6]:checked").val();
     var user7 = $("input[name=question7]:checked").val();
+    var user8 = $("input[name=question8]:checked").val();
+    var user9 = $("input[name=question9]:checked").val();
+    var user10 = $("input[name=question10]:checked").val();
+    var user11 = $("input[name=question11]:checked").val();
+    var user12 = $("input[name=question12]:checked").val();
+    var user13 = $("input[name=question13]:checked").val();
+    var user14 = $("input[name=question14]:checked").val();
+    var user15 = $("input[name=question15]:checked").val();
+    var user16 = $("input[name=question16]:checked").val();
+    var user17 = $("input[name=question17]:checked").val();
+    var user18 = $("input[name=question18]:checked").val();
+    var user19 = $("input[name=question19]:checked").val();
+    var user20 = $("input[name=question20]:checked").val();
+    var user21 = $("input[name=question21]:checked").val();
+    var user22 = $("input[name=question22]:checked").val();
+    var user23 = $("input[name=question23]:checked").val();
+    var user24 = $("input[name=question24]:checked").val();
+    var user25 = $("input[name=question25]:checked").val();
+    var user26 = $("input[name=question26]:checked").val();
+    var user27 = $("input[name=question27]:checked").val();
+    var user28 = $("input[name=question28]:checked").val();
+    var user29 = $("input[name=question29]:checked").val();
+    var user30 = $("input[name=question30]:checked").val();
+    var user31 = $("input[name=question31]:checked").val();
+    var user32 = $("input[name=question32]:checked").val();
+    var user33 = $("input[name=question33]:checked").val();
+    var user34 = $("input[name=question34]:checked").val();
+    var user35 = $("input[name=question35]:checked").val();
+    var user36 = $("input[name=question36]:checked").val();
+    var user37 = $("input[name=question37]:checked").val();
+    var user38 = $("input[name=question38]:checked").val();
+    var user39 = $("input[name=question39]:checked").val();
+    var user40 = $("input[name=question40]:checked").val();
+    var user41 = $("input[name=question41]:checked").val();
+    var user42 = $("input[name=question42]:checked").val();
+    var user43 = $("input[name=question43]:checked").val();
+    var user44 = $("input[name=question44]:checked").val();
+    var user45 = $("input[name=question45]:checked").val();
+    var user46 = $("input[name=question46]:checked").val();
+    var user47 = $("input[name=question47]:checked").val();
+    var user48 = $("input[name=question48]:checked").val();
+    var user49 = $("input[name=question49]:checked").val();
+    var user50 = $("input[name=question50]:checked").val();
+    var user51 = $("input[name=question51]:checked").val();
+    var user52 = $("input[name=question52]:checked").val();
+    var user53 = $("input[name=question53]:checked").val();
+    var user54 = $("input[name=question54]:checked").val();
+    var user55 = $("input[name=question55]:checked").val();
+    var user56 = $("input[name=question56]:checked").val();
+    var user57 = $("input[name=question57]:checked").val();
+    var user58 = $("input[name=question58]:checked").val();
+    var user59 = $("input[name=question59]:checked").val();
+    var user60 = $("input[name=question60]:checked").val();
+    var user61 = $("input[name=question61]:checked").val();
+    var user62 = $("input[name=question62]:checked").val();
+    var user63 = $("input[name=question63]:checked").val();
+    var user64 = $("input[name=question64]:checked").val();
+    var user65 = $("input[name=question65]:checked").val();
+    var user66 = $("input[name=question66]:checked").val();
+    var user67 = $("input[name=question67]:checked").val();
+    var user68 = $("input[name=question68]:checked").val();
+    var user69 = $("input[name=question69]:checked").val();
+    var user70 = $("input[name=question70]:checked").val();
 
-    alert(user1+user2+user3+user4+user5+user6+user7);
+    var userEI = [user1, user8, user15, user22, user29, user36, user43, user50, user57, user64];
+
+    var userSN = [user2, user3, user9, user10, user16, user17, user23, user24, user30, user31, user37, user38, user44, user45, user51, user52, user58, user59, user65, user66];
+
+    var userTF = [user4, user5, user11, user12, user18, user19, user25, user26, user32, user33, user39, user40, user46, user47, user53, user54, user60, user61, user67, user68];
+
+    var userJP = [user6, user7, user13, user14, user20, user21, user27, user28, user34, user35, user41, user42, user48, user49, user55, user56, user62, user63, user69, user70];
+
+    console.log(userEI);
+    console.log(userSN);
+    console.log(userTF);
+    console.log(userJP);
+
   });
+  
 });
