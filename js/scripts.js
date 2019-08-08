71d6289816c96dd0a2c07c7194432cccf7312d3c
Suzi Rubino <suzi@suzirubi.com>
2019-08-08 13:52:33 -0700

    Add javascript for imagery types

--- js/scripts.js
+++ js/scripts.js
@@ -1,98 +1,81 @@
 $(document).ready(function(){
   $("form#test").submit(function(event){
     event.preventDefault();
-    var user1 = $("input[name=question1]:checked").val();
-    var user2 = $("input[name=question2]:checked").val();
-    var user3 = $("input[name=question3]:checked").val();
-    var user4 = $("input[name=question4]:checked").val();
-    var user5 = $("input[name=question5]:checked").val();
-    var user6 = $("input[name=question6]:checked").val();
-    var user7 = $("input[name=question7]:checked").val();
-    var user8 = $("input[name=question8]:checked").val();
-    var user9 = $("input[name=question9]:checked").val();
-    var user10 = $("input[name=question10]:checked").val();
-    var user11 = $("input[name=question11]:checked").val();
-    var user12 = $("input[name=question12]:checked").val();
-    var user13 = $("input[name=question13]:checked").val();
-    var user14 = $("input[name=question14]:checked").val();
-    var user15 = $("input[name=question15]:checked").val();
-    var user16 = $("input[name=question16]:checked").val();
-    var user17 = $("input[name=question17]:checked").val();
-    var user18 = $("input[name=question18]:checked").val();
-    var user19 = $("input[name=question19]:checked").val();
-    var user20 = $("input[name=question20]:checked").val();
-    var user21 = $("input[name=question21]:checked").val();
-    var user22 = $("input[name=question22]:checked").val();
-    var user23 = $("input[name=question23]:checked").val();
-    var user24 = $("input[name=question24]:checked").val();
-    var user25 = $("input[name=question25]:checked").val();
-    var user26 = $("input[name=question26]:checked").val();
-    var user27 = $("input[name=question27]:checked").val();
-    var user28 = $("input[name=question28]:checked").val();
-    var user29 = $("input[name=question29]:checked").val();
-    var user30 = $("input[name=question30]:checked").val();
-    var user31 = $("input[name=question31]:checked").val();
-    var user32 = $("input[name=question32]:checked").val();
-    var user33 = $("input[name=question33]:checked").val();
-    var user34 = $("input[name=question34]:checked").val();
-    var user35 = $("input[name=question35]:checked").val();
-    var user36 = $("input[name=question36]:checked").val();
-    var user37 = $("input[name=question37]:checked").val();
-    var user38 = $("input[name=question38]:checked").val();
-    var user39 = $("input[name=question39]:checked").val();
-    var user40 = $("input[name=question40]:checked").val();
-    var user41 = $("input[name=question41]:checked").val();
-    var user42 = $("input[name=question42]:checked").val();
-    var user43 = $("input[name=question43]:checked").val();
-    var user44 = $("input[name=question44]:checked").val();
-    var user45 = $("input[name=question45]:checked").val();
-    var user46 = $("input[name=question46]:checked").val();
-    var user47 = $("input[name=question47]:checked").val();
-    var user48 = $("input[name=question48]:checked").val();
-    var user49 = $("input[name=question49]:checked").val();
-    var user50 = $("input[name=question50]:checked").val();
-    var user51 = $("input[name=question51]:checked").val();
-    var user52 = $("input[name=question52]:checked").val();
-    var user53 = $("input[name=question53]:checked").val();
-    var user54 = $("input[name=question54]:checked").val();
-    var user55 = $("input[name=question55]:checked").val();
-    var user56 = $("input[name=question56]:checked").val();
-    var user57 = $("input[name=question57]:checked").val();
-    var user58 = $("input[name=question58]:checked").val();
-    var user59 = $("input[name=question59]:checked").val();
-    var user60 = $("input[name=question60]:checked").val();
-    var user61 = $("input[name=question61]:checked").val();
-    var user62 = $("input[name=question62]:checked").val();
-    var user63 = $("input[name=question63]:checked").val();
-    var user64 = $("input[name=question64]:checked").val();
-    var user65 = $("input[name=question65]:checked").val();
-    var user66 = $("input[name=question66]:checked").val();
-    var user67 = $("input[name=question67]:checked").val();
-    var user68 = $("input[name=question68]:checked").val();
-    var user69 = $("input[name=question69]:checked").val();
-    var user70 = $("input[name=question70]:checked").val();
+    var personalityStatement = $("select#type").val();
+    var gender = $("input:radio[name=gender]:checked").val();
+    var age = parseInt($("input#age").val());
 
-    var userEI = [user1, user8, user15, user22, user29, user36, user43, user50, user57, user64];
+    console.log(personalityStatement);
+    console.log(gender);
+    console.log(age);
+    $("div#showType").show();
+    $("p#picturePara").empty();
+    $("form#test").hide();
+    $("p#introParagraph").hide();
 
+    if (age <= 5) {
+      $("p#picturePara").text("Come on, that's not your right age");
+      $("img#imageType").removeAttr("src alt").attr({"src": "img/monster.jpg", "alt": "picture of stranger things monster"});
+    } else if (personalityStatement === "ISFP" && gender === "female") {
+      $("p#picturePara").text("Appropriate text here, Julia?");
+      $("img#imageType").removeAttr("src alt").attr({"src": "img/elevenAdventurerkISFP800x1000.png", "alt": "eleven with bloody nose"});
+    } else if (personalityStatement === "ISFP" && gender !== "female") {
+      $("p#picturePara").text("Appropriate text here, Julia?");
+      $("img#imageType").removeAttr("src alt").attr({"src": "img/jonathonLogicianINFP790x100.png", "alt": "jonathon the logician"});
+    } else if (personalityStatement === "INFJ" && gender === "female") {
+      $("p#picturePara").text("Appropriate text here, Julia?");
+      $("img#imageType").removeAttr("src alt").attr({"src": "img/suzieINFJ.png", "alt": "suzie"});
+    } else if (personalityStatement === "INFJ" && gender !== "female") {
+      $("p#picturePara").text("Appropriate text here, Julia?");
+      $("img#imageType").removeAttr("src alt").attr({"src": "img/wheelerAdvocateINFJ845x1000.png", "alt": "mike wheeler the advocate"});
+    } else if (personalityStatement === "ESTJ" && gender === "female") {
+      $("p#picturePara").text("Appropriate text here, Julia?");
+      $("img#imageType").removeAttr("src alt").attr({"src": "img/florenceESTJ.png", "alt": "florence"});
+    } else if (personalityStatement === "ESTJ" && gender !== "female") {
+      $("p#picturePara").text("Appropriate text here, Julia?");
+      $("img#imageType").removeAttr("src alt").attr({"src": "img/sinclairExecutiveESTJ800x1000.png", "alt": "sinclair"});
+    } else if (personalityStatement === "ISFJ" && gender === "female") {
+      $("p#picturePara").text("Appropriate text here, Julia?");
+      $("img#imageType").removeAttr("src alt").attr({"src": "img/joyceDefenderISFJ662x1000.png", "alt": "joyce"});
+    } else if (personalityStatement === "ISFJ" && gender !== "female") {
+      $("p#picturePara").text("Personality okay, not quite a ISFJ...Appropriate text also");
+      $("img#imageType").removeAttr("src alt").attr({"src": "img/jonathonLogicianINFP790x100.png", "alt": "jonathon the logician"});
+    } else if (personalityStatement === "ENTP" && gender === "female") {
+      $("p#picturePara").text("Appropriate text here, Julia?");
+      $("img#imageType").removeAttr("src alt").attr({"src": "img/joyceDefenderISFJ662x1000.png", "alt": "joyce"});
+    } else if (personalityStatement === "ENTP" && gender !== "female") {
+      $("p#picturePara").text("Appropriate text also");
+      $("img#imageType").removeAttr("src alt").attr({"src": "img/dustinDebaterENTP833x1000.png", "alt": "dustin"});
+    } else if (personalityStatement === "ESFJ" && gender === "female") {
+      $("p#picturePara").text("Appropriate text here, Julia?");
+      $("img#imageType").removeAttr("src alt").attr({"src": "img/nancyConsulESFJ800x1000.png", "alt": "nancy"});
+    } else if (personalityStatement === "ESFJ" && gender !== "female") {
+      $("p#picturePara").text("Appropriate text also");
+      $("img#imageType").removeAttr("src alt").attr({"src": "img/newby.png", "alt": "newby"});
+    } else if ((personalityStatement === "INFP" || personalityStatement === "INFP2") && gender === "female") {
+      $("p#picturePara").text("Appropriate text here, Julia?");
+      $("img#imageType").removeAttr("src alt").attr({"src": "img/elevenAdventurerkISFP800x1000.png", "alt": "eleven with bloody nose"});
+    } else if (personalityStatement === "INFP" && gender !== "female") {
+      $("p#picturePara").text("Appropriate text also");
+      $("img#imageType").removeAttr("src alt").attr({"src": "img/willMediatorINFP800x1000.png", "alt": "newby"});
+    } else if (personalityStatement === "INFP2" && gender !== "female") {
+      $("p#picturePara").text("Personality okay, not quite a ISFJ...Appropriate text also");
+      $("img#imageType").removeAttr("src alt").attr({"src": "img/jonathonLogicianINFP790x100.png", "alt": "jonathon the logician"});
+    } else if (personalityStatement === "ISTP" && gender === "female") {
+      $("p#picturePara").text("Appropriate text here, Julia?");
+      $("img#imageType").removeAttr("src alt").attr({"src": "img/maxISTP.png", "alt": "max"});
+    } else if (personalityStatement === "ISTP" && gender !== "female") {
+      $("p#picturePara").text("Appropriate text also");
+      $("img#imageType").removeAttr("src alt").attr({"src": "img/jimVirtuosoISTP887x1000.png", "alt": "jim the virtuoso"});
+    } else {
+      $("p#picturePara").text("Something strange happened, you should try again with different answers");
+      $("img#imageType").removeAttr("src alt").attr({"src": "img/monster.jpg", "alt": "picture of stranger things monster"});
+    }
+  });
 
-
-    var userSN = [user2, user3, user9, user10, user16, user17, user23, user24, user30, user31, user37, user38, user44, user45, user51, user52, user58, user59, user65, user66];
-
-    var userTF = [user4, user5, user11, user12, user18, user19, user25, user26, user32, user33, user39, user40, user46, user47, user53, user54, user60, user61, user67, user68];
-
-    var userJP = [user6, user7, user13, user14, user20, user21, user27, user28, user34, user35, user41, user42, user48, user49, user55, user56, user62, user63, user69, user70];
-
-    console.log(userEI);
-    console.log(userSN);
-    console.log(userTF);
-    console.log(userJP);
-
-    userEI.forEach(function(answer) {
-      var userInput = $("input." + blank).val();
-      console.log(userInput);
-      $("." + blank).text(userInput).val();
-    });
-
+  $("button#tryAgain").click(function(){
+      $("form#test").show();
+      $("div#showType").hide();
   });
 
 });
